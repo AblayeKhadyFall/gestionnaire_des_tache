2427201a1c52d5fa2b09b6bde25ae0bf3f1eeaad
kanebassirou <kaneb1927@gmail.com>
2024-03-24 00:06:07 +0000

    aptation au different taille d'ecran

--- lib/views/ajout_tache.dart
+++ lib/views/ajout_tache.dart
@@ -25,128 +25,131 @@ class _AddEditTacheScreenState extends State<AddEditTacheScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: titreController,
-                decoration: const InputDecoration(
-                  labelText: 'Titre',
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer un titre';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une description';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: dateDebutController,
-                decoration: const InputDecoration(
-                  labelText: 'Date de début',
-                  suffixIcon: Icon(Icons.calendar_today),
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une date de début';
-                  }
-                  return null;
-                },
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (selectedDate != null) {
-                    dateDebutController.text = selectedDate.toString();
-                  }
-                },
-              ),
-              TextFormField(
-                controller: dateFinController,
-                decoration: const InputDecoration(
-                  labelText: 'Date de fin',
-                  suffixIcon: Icon(Icons.calendar_today),
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une date de fin';
-                  }
-                  return null;
-                },
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (selectedDate != null) {
-                    dateFinController.text = selectedDate.toString();
-                  }
-                },
-                // Ajoutez un icône de calendrier ici
-                // Par exemple : suffixIcon: Icon(Icons.calendar_today),
-              ),
-              DropdownButtonFormField<EtatTache>(
-                decoration: InputDecoration(labelText: 'État de la tâche'),
-                value: _etatSelectionne,
-                onChanged: (EtatTache? nouvelEtat) {
-                  setState(() {
-                    _etatSelectionne = nouvelEtat;
-                  });
-                },
-                items:
-                    EtatTache.values.map<DropdownMenuItem<EtatTache>>((etat) {
-                  return DropdownMenuItem<EtatTache>(
-                    value: etat,
-                    child: Text(etat.toString().split('.').last),
-                  );
-                }).toList(),
-                validator: (value) =>
-                    value == null ? 'Veuillez sélectionner un état' : null,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    Tache nouvelleTache = Tache(
-                      id: DateTime.now().toString(),
-                      titre: titreController.text,
-                      description: descriptionController.text,
-                      dateDebut: dateDebutController.text,
-                      dateFin: dateFinController.text,
-                      etat: _etatSelectionne ?? EtatTache.pasDebute,
-                    );
-
-                    try {
-                      await DatabaseService().ajouterTache(nouvelleTache);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("Tâche ajoutée avec succès")));
-                      Navigator.of(context).pop();
-                    } catch (e) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("Erreur lors de l'ajout de la tâche")));
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: titreController,
+                  decoration: const InputDecoration(
+                    labelText: 'Titre',
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer un titre';
                     }
-                  }
-                },
-                child: Text('Enregistrer'),
-              ),
-            ],
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une description';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: dateDebutController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date de début',
+                    suffixIcon: Icon(Icons.calendar_today),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une date de début';
+                    }
+                    return null;
+                  },
+                  onTap: () async {
+                    DateTime? selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                    if (selectedDate != null) {
+                      dateDebutController.text = selectedDate.toString();
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: dateFinController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date de fin',
+                    suffixIcon: Icon(Icons.calendar_today),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une date de fin';
+                    }
+                    return null;
+                  },
+                  onTap: () async {
+                    DateTime? selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                    if (selectedDate != null) {
+                      dateFinController.text = selectedDate.toString();
+                    }
+                  },
+                  // Ajoutez un icône de calendrier ici
+                  // Par exemple : suffixIcon: Icon(Icons.calendar_today),
+                ),
+                DropdownButtonFormField<EtatTache>(
+                  decoration: InputDecoration(labelText: 'État de la tâche'),
+                  value: _etatSelectionne,
+                  onChanged: (EtatTache? nouvelEtat) {
+                    setState(() {
+                      _etatSelectionne = nouvelEtat;
+                    });
+                  },
+                  items:
+                      EtatTache.values.map<DropdownMenuItem<EtatTache>>((etat) {
+                    return DropdownMenuItem<EtatTache>(
+                      value: etat,
+                      child: Text(etat.toString().split('.').last),
+                    );
+                  }).toList(),
+                  validator: (value) =>
+                      value == null ? 'Veuillez sélectionner un état' : null,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      Tache nouvelleTache = Tache(
+                        id: DateTime.now().toString(),
+                        titre: titreController.text,
+                        description: descriptionController.text,
+                        dateDebut: dateDebutController.text,
+                        dateFin: dateFinController.text,
+                        etat: _etatSelectionne ?? EtatTache.pasDebute,
+                      );
+
+                      try {
+                        await DatabaseService().ajouterTache(nouvelleTache);
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Tâche ajoutée avec succès")));
+                        Navigator.of(context).pop();
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content:
+                                Text("Erreur lors de l'ajout de la tâche")));
+                      }
+                    }
+                  },
+                  child: Text('Enregistrer'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/views/editer_tache.dart
+++ lib/views/editer_tache.dart
@@ -41,149 +41,152 @@ class _EditTacheScreenState extends State<EditTacheScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: titreController,
-                decoration: const InputDecoration(
-                  labelText: 'Titre',
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer un titre';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: descriptionController,
-                decoration: const InputDecoration(
-                  labelText: 'Description',
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une description';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: dateDebutController,
-                decoration: const InputDecoration(
-                  labelText: 'Date de début',
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une date de début';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: dateDebutController,
-                decoration: const InputDecoration(
-                  labelText: 'Date de début',
-                  suffixIcon: Icon(Icons.calendar_today),
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une date de début';
-                  }
-                  return null;
-                },
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (selectedDate != null) {
-                    dateDebutController.text = selectedDate.toString();
-                  }
-                },
-              ),
-              TextFormField(
-                controller: dateFinController,
-                decoration: const InputDecoration(
-                  labelText: 'Date de fin',
-                  suffixIcon: Icon(Icons.calendar_today),
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Veuillez entrer une date de fin';
-                  }
-                  return null;
-                },
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (selectedDate != null) {
-                    dateFinController.text = selectedDate.toString();
-                  }
-                },
-              ),
-              DropdownButtonFormField<EtatTache>(
-                decoration: const InputDecoration(
-                  labelText: 'État de la tâche',
-                ),
-                value: _etatSelectionne,
-                onChanged: (EtatTache? nouvelEtat) {
-                  setState(() {
-                    _etatSelectionne = nouvelEtat;
-                  });
-                },
-                items:
-                    EtatTache.values.map<DropdownMenuItem<EtatTache>>((etat) {
-                  return DropdownMenuItem<EtatTache>(
-                    value: etat,
-                    child: Text(etat.toString().split('.').last),
-                  );
-                }).toList(),
-                validator: (value) =>
-                    value == null ? 'Veuillez sélectionner un état' : null,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    Tache tacheModifiee = Tache(
-                      id: widget.tacheToEdit.id,
-                      titre: titreController.text,
-                      description: descriptionController.text,
-                      dateDebut: dateDebutController.text,
-                      dateFin: dateFinController.text,
-                      etat: _etatSelectionne ?? EtatTache.pasDebute,
-                    );
-
-                    try {
-                      await Provider.of<DatabaseService>(context, listen: false)
-                          .mettreAJourTache(tacheModifiee);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Tâche modifiée avec succès"),
-                        ),
-                      );
-                      Navigator.of(context).pop();
-                    } catch (e) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            "Erreur lors de la modification de la tâche: $e",
-                          ),
-                        ),
-                      );
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: titreController,
+                  decoration: const InputDecoration(
+                    labelText: 'Titre',
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer un titre';
                     }
-                  }
-                },
-                child: const Text('Modifier'),
-              ),
-            ],
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: descriptionController,
+                  decoration: const InputDecoration(
+                    labelText: 'Description',
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une description';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: dateDebutController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date de début',
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une date de début';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: dateDebutController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date de début',
+                    suffixIcon: Icon(Icons.calendar_today),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une date de début';
+                    }
+                    return null;
+                  },
+                  onTap: () async {
+                    DateTime? selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                    if (selectedDate != null) {
+                      dateDebutController.text = selectedDate.toString();
+                    }
+                  },
+                ),
+                TextFormField(
+                  controller: dateFinController,
+                  decoration: const InputDecoration(
+                    labelText: 'Date de fin',
+                    suffixIcon: Icon(Icons.calendar_today),
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Veuillez entrer une date de fin';
+                    }
+                    return null;
+                  },
+                  onTap: () async {
+                    DateTime? selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    );
+                    if (selectedDate != null) {
+                      dateFinController.text = selectedDate.toString();
+                    }
+                  },
+                ),
+                DropdownButtonFormField<EtatTache>(
+                  decoration: const InputDecoration(
+                    labelText: 'État de la tâche',
+                  ),
+                  value: _etatSelectionne,
+                  onChanged: (EtatTache? nouvelEtat) {
+                    setState(() {
+                      _etatSelectionne = nouvelEtat;
+                    });
+                  },
+                  items:
+                      EtatTache.values.map<DropdownMenuItem<EtatTache>>((etat) {
+                    return DropdownMenuItem<EtatTache>(
+                      value: etat,
+                      child: Text(etat.toString().split('.').last),
+                    );
+                  }).toList(),
+                  validator: (value) =>
+                      value == null ? 'Veuillez sélectionner un état' : null,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      Tache tacheModifiee = Tache(
+                        id: widget.tacheToEdit.id,
+                        titre: titreController.text,
+                        description: descriptionController.text,
+                        dateDebut: dateDebutController.text,
+                        dateFin: dateFinController.text,
+                        etat: _etatSelectionne ?? EtatTache.pasDebute,
+                      );
+
+                      try {
+                        await Provider.of<DatabaseService>(context,
+                                listen: false)
+                            .mettreAJourTache(tacheModifiee);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Tâche modifiée avec succès"),
+                          ),
+                        );
+                        Navigator.of(context).pop();
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Erreur lors de la modification de la tâche: $e",
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Modifier'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
